f2ce8f18a17952c10235f51f4e1d39f04e5ca919
Scott Addie <scott.addie@microsoft.com>
2025-10-05 17:43:14 -0500

    Fix code

--- Program.cs
+++ Program.cs
@@ -1,30 +1,53 @@
 ﻿using Azure.Identity;
 using OpenAI;
 using OpenAI.Responses;
+using System.ClientModel;
 using System.ClientModel.Primitives;
 
 #pragma warning disable OPENAI001
 
-// DEMO 1
-await UseAzureOpenAiResponses();
+// DEMO 1.1: Azure OpenAI Responses w/ Entra or API key
+//await UseAzureOpenAIResponses();
+// DEMO 1.2: OpenAI Responses w/ API key
+await UseOpenAiResponses();
 
-// DEMO 2
-await UseOpenAiResponsesWithRemoteMcpTool();
+// DEMO 2: OpenAI Responses w/ Stripe MCP tool
+//await UseOpenAIResponsesWithRemoteMcpTool();
 
 static async Task UseAzureOpenAiResponses()
 {
-    OpenAIResponseClient client = GetAzureOpenAiResponseClient(useEntra: false);
-    OpenAIResponse response = await client.CreateResponseAsync("Say 'This is a test.'");
+    OpenAIClient openAIClient = GetOpenAIClient(false);
+    OpenAIResponseClient responseClient = openAIClient.GetOpenAIResponseClient("codex-mini-2");
+    OpenAIResponse response = await responseClient.CreateResponseAsync("Say 'This is a test.'");
 
     Console.WriteLine(response.GetOutputText());
 }
 
-static OpenAIResponseClient GetAzureOpenAiResponseClient(bool useEntra)
+static async Task UseOpenAiResponses()
+{
+    string apiKey = GetOpenAiApiKey()!;
+    OpenAIClient openAIClient = new(
+        credential: new ApiKeyCredential(apiKey));
+    Console.WriteLine("Using OpenAI API key");
+    OpenAIResponseClient responseClient = openAIClient.GetOpenAIResponseClient("gpt-5");
+    OpenAIResponse response = await responseClient.CreateResponseAsync("Say 'This is a test.'");
+
+    Console.WriteLine(response.GetOutputText());
+}
+
+static OpenAIClient GetOpenAIClient(bool useEntra)
 {
     const string azureOpenAiEndpoint = "https://openai-jc4hwqn6g2r7g.openai.azure.com";
+    OpenAIClient client = null!;
+
+    OpenAIClientOptions clientOptions = new()
+    {
+        Endpoint = new Uri($"{azureOpenAiEndpoint}/openai/v1/"),
+    };
 
     if (useEntra)
     {
+        Console.WriteLine("Using Entra ID");
         VisualStudioCredentialOptions credentialOptions = new()
         {
             TenantId = Environment.GetEnvironmentVariable("AZURE_TENANT_ID"),
@@ -34,20 +57,20 @@ static OpenAIResponseClient GetAzureOpenAiResponseClient(bool useEntra)
             tokenProvider: credential,
             scope: "https://cognitiveservices.azure.com/.default");
 
-        OpenAIClientOptions clientOptions = new()
-        {
-            Endpoint = new Uri($"{azureOpenAiEndpoint}/openai/v1/"),
-        };
-
-        return new(
-            model: "codex-mini-2",
+        client = new(
             authenticationPolicy: tokenPolicy,
             options: clientOptions);
     }
+    else
+    {
+        Console.WriteLine("Using Azure OpenAI API key");
+        string apiKey = GetAzureOpenAiApiKey()!;
+        client = new(
+            credential: new ApiKeyCredential(apiKey),
+            options: clientOptions);
+    }
 
-    return new(
-        model: "codex-mini-2",
-        apiKey: GetOpenAiApiKey());
+    return client;
 }
 
 static async Task UseOpenAiResponsesWithRemoteMcpTool()
@@ -90,6 +113,17 @@ static async Task UseOpenAiResponsesWithRemoteMcpTool()
 }
 
 #pragma warning restore OPENAI001
+
+static string? GetAzureOpenAiApiKey()
+{
+    string? apiKey = Environment.GetEnvironmentVariable("AZURE_OPENAI_API_KEY");
+    if (string.IsNullOrEmpty(apiKey))
+    {
+        Console.WriteLine("Error: AZURE_OPENAI_API_KEY environment variable is not set.");
+        return null;
+    }
+    return apiKey;
+}
 
 static string? GetOpenAiApiKey()
 {
